2256f9590736fe9949b9b01a84faa332b048ce89
LinusWase <linusmwase@gmail.com>
2025-10-03 17:25:37 +0200

    Started on a new course

---
+++ Part1/printing/ada_lovelace/ada_lovelace.java
@@ -0,0 +1,5 @@
+public class ada_lovelace {
+    public static void main(String[] args) {
+        System.out.println("Ada Lovelace");
+    }
+}
---
+++ Part1/printing/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+        System.out.println("Hi again");
+        /*
+        Long
+        Long
+        Long
+        Comment
+         */
+    }
+}
